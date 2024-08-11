2887e980e53ff3ec3e34484ca7edf3ff2cb7bd10
HazimNabil <HazemNabil95@outlook.com>
2024-08-11 02:53:17 +0200

    Add featured book item to home view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/featured_book_item.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
@@ -10,6 +11,7 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        FeaturedBookItem(),
       ],
     );
   }
