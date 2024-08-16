4be660a200e847d540520b20833f48e3a202753c
HazimNabil <HazemNabil95@outlook.com>
2024-08-16 02:41:34 +0200

    Change app bar color and action icon

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -14,6 +14,8 @@ class BookDetailsView extends StatelessWidget {
 
   AppBar buildAppBar() {
     return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
       leading: IconButton(
         onPressed: () {},
         icon: const Icon(Icons.close),
@@ -21,7 +23,7 @@ class BookDetailsView extends StatelessWidget {
       actions: [
         IconButton(
           onPressed: () {},
-          icon: const Icon(Icons.shopping_cart),
+          icon: const Icon(Icons.shopping_cart_outlined),
         )
       ],
     );
