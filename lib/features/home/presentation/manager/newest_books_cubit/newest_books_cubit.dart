6e532e3e180d74d2815005eb9b3283773439e7dc
HazimNabil <HazemNabil95@outlook.com>
2024-08-18 00:32:34 +0200

    Create newest books states

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../data/models/book/book.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit() : super(NewestBooksInitial());
+}
