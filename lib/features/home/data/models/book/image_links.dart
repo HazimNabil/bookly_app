f10686e004f93923a5a6690a70139d9f39b477e7
HazimNabil <HazemNabil95@outlook.com>
2024-08-18 22:45:00 +0200

    Make the model volume info non nullable

--- lib/features/home/data/models/book/image_links.dart
+++ lib/features/home/data/models/book/image_links.dart
@@ -1,20 +1,24 @@
 import 'package:equatable/equatable.dart';
 
 class ImageLinks extends Equatable {
-  final String? smallThumbnail;
-  final String? thumbnail;
+  final String smallThumbnail;
+  final String thumbnail;
 
-  const ImageLinks({this.smallThumbnail, this.thumbnail});
+  const ImageLinks({required this.smallThumbnail, required this.thumbnail});
 
-  factory ImageLinks.fromJson(Map<String, dynamic> json) => ImageLinks(
-        smallThumbnail: json['smallThumbnail'] as String?,
-        thumbnail: json['thumbnail'] as String?,
-      );
+  factory ImageLinks.fromJson(Map<String, dynamic> json) {
+    return ImageLinks(
+      smallThumbnail: json['smallThumbnail'] as String,
+      thumbnail: json['thumbnail'] as String,
+    );
+  }
 
-  Map<String, dynamic> toJson() => {
-        'smallThumbnail': smallThumbnail,
-        'thumbnail': thumbnail,
-      };
+  Map<String, dynamic> toJson() {
+    return {
+      'smallThumbnail': smallThumbnail,
+      'thumbnail': thumbnail,
+    };
+  }
 
   @override
   List<Object?> get props => [smallThumbnail, thumbnail];
