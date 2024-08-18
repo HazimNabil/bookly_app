c2597b2179c883bca3874cbd75f40b92345049d2
HazimNabil <HazemNabil95@outlook.com>
2024-08-18 20:41:31 +0200

    Provide featured and newest books cubits

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,4 +1,10 @@
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'widgets/home_view_body.dart';
 
@@ -7,8 +13,22 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeViewBody(),
+    return Scaffold(
+      body: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => FeaturedBooksCubit(
+              HomeRepoImpl(ApiService(Dio())),
+            ),
+          ),
+          BlocProvider(
+            create: (context) => NewestBooksCubit(
+              HomeRepoImpl(ApiService(Dio())),
+            ),
+          ),
+        ],
+        child: const HomeViewBody(),
+      ),
     );
   }
 }
