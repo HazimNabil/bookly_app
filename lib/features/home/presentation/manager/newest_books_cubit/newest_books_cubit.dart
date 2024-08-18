e4970eb9cc61c360dfba77f50757ce2c08b4ace5
HazimNabil <HazemNabil95@outlook.com>
2024-08-18 20:36:56 +0200

    Import flutter_bloc instead of bloc

--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
