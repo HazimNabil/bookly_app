d594f39f83f93ca1adc1e41ed0e6e7d47fc91557
HazimNabil <HazemNabil95@outlook.com>
2024-08-18 22:08:53 +0200

    Build featured books list view with cubit

---
+++ lib/core/widgets/something_went_wrong.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class SomethingWentWrong extends StatelessWidget {
+  final String errorMessage;
+
+  const SomethingWentWrong({super.key, required this.errorMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      errorMessage,
+      style: Styles.textStyle18.copyWith(
+        color: Colors.red,
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_books_list_view.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/core/widgets/loading_indicator.dart';
+import 'package:bookly_app/core/widgets/something_went_wrong.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'custom_book_image.dart';
 
@@ -7,15 +11,31 @@ class FeaturedBooksListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.25,
-        child: ListView.builder(
-          padding: const EdgeInsets.only(left: 16),
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) => const CustomBookImage(),
-        ),
-      ),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SliverToBoxAdapter(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.25,
+              child: ListView.builder(
+                padding: const EdgeInsets.only(left: 16),
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) => const CustomBookImage(),
+              ),
+            ),
+          );
+        } else if (state is FeaturedBooksFailure) {
+          return SliverToBoxAdapter(
+            child: SomethingWentWrong(
+              errorMessage: state.errorMessage,
+            ),
+          );
+        } else {
+          return const SliverToBoxAdapter(
+            child: LoadingIndicator(),
+          );
+        }
+      },
     );
   }
 }
