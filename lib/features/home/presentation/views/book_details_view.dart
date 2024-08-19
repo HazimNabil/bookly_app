a037c83ddf3df8a40db61d4ea717a376faa6e37e
HazimNabil <HazemNabil95@outlook.com>
2024-08-19 03:34:04 +0200

    Provide similar books cubit

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -1,5 +1,9 @@
+import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BookDetailsView extends StatelessWidget {
   const BookDetailsView({super.key});
@@ -8,7 +12,10 @@ class BookDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: const BookDetailsViewBody(),
+      body: BlocProvider(
+        create: (context) => SimilarBooksCubit(getIt.get<HomeRepoImpl>()),
+        child: const BookDetailsViewBody(),
+      ),
     );
   }
 
