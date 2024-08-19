1458ce604195f007efc2a549d0621dc39ff46cd9
HazimNabil <HazemNabil95@outlook.com>
2024-08-19 03:23:57 +0200

    Create similar books states

---
+++ lib/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bookly_app/features/home/data/models/book/book.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'similar_books_state.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksState> {
+  SimilarBooksCubit() : super(SimilarBooksInitial());
+}
