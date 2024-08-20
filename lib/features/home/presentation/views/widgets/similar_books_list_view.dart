fbdc5bc5abf998f227cc4935db33829b337d4cb5
HazimNabil <HazemNabil95@outlook.com>
2024-08-21 00:17:09 +0200

    Navigate to details view from similar books

--- lib/features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,8 +1,10 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/widgets/loading_indicator.dart';
 import 'package:bookly_app/core/widgets/something_went_wrong.dart';
 import 'package:bookly_app/features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../core/widgets/custom_book_image.dart';
 
@@ -19,11 +21,19 @@ class SimilarBooksListView extends StatelessWidget {
             child: ListView.builder(
               padding: const EdgeInsets.only(left: 16),
               scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) => CustomBookImage(
-                imageUrl: state
-                        .similarBooks[index].volumeInfo.imageLinks?.thumbnail ??
-                    '',
-              ),
+              itemBuilder: (context, index) {
+                var thumbnail =
+                    state.similarBooks[index].volumeInfo.imageLinks?.thumbnail;
+                return GestureDetector(
+                  onTap: () => context.push(
+                    AppRouter.kBookDetailsPath,
+                    extra: state.similarBooks[index],
+                  ),
+                  child: CustomBookImage(
+                    imageUrl: thumbnail ?? '',
+                  ),
+                );
+              },
             ),
           );
         } else if (state is SimilarBooksFailure) {
