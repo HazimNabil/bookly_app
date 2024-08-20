4f11d08fe4898d31d8d1ef91f347333f22722610
HazimNabil <HazemNabil95@outlook.com>
2024-08-21 00:42:57 +0200

    Decrease book image border radius

--- lib/core/widgets/custom_book_image.dart
+++ lib/core/widgets/custom_book_image.dart
@@ -11,7 +11,7 @@ class CustomBookImage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(right: 12.0),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(16),
+        borderRadius: BorderRadius.circular(10),
         child: AspectRatio(
           aspectRatio: 2.7 / 4,
           child: CachedNetworkImage(
