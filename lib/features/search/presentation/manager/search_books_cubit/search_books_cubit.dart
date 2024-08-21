fecb4ec2f85907d7d7d09d8a25a98f6101cd49f9
HazimNabil <HazemNabil95@outlook.com>
2024-08-21 02:01:32 +0200

    Create search_books cubit method

--- lib/features/search/presentation/manager/search_books_cubit/search_books_cubit.dart
+++ lib/features/search/presentation/manager/search_books_cubit/search_books_cubit.dart
@@ -1,9 +1,21 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly_app/core/models/book/book.dart';
+import 'package:bookly_app/features/search/data/repos/search_repo.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'search_books_state.dart';
 
 class SearchBooksCubit extends Cubit<SearchBooksState> {
-  SearchBooksCubit() : super(SearchBooksInitial());
+  final SearchRepo searchRepo;
+
+  SearchBooksCubit(this.searchRepo) : super(SearchBooksInitial());
+
+  Future<void> searchBooks(String title) async {
+    emit(SearchBooksLoading());
+    var result = await searchRepo.searchBooks(title);
+    result.fold(
+      (failure) => emit(SearchBooksFailure(failure.errorMessage)),
+      (books) => emit(SearchBooksSuccess(books)),
+    );
+  }
 }
