1377be11df58b5ffad0b8296242aaa0bc124e75b
HazimNabil <HazemNabil95@outlook.com>
2024-08-21 20:46:56 +0200

    Add base and highlight colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF100B20);
+const kShimmerBaseColor = Color(0xFF171525);
+const kShimmerHighlightColor = Color(0xFF1E1C33);
 const kTransitionDuration = Duration(milliseconds: 250);
 const kGtSectraFine = 'GT Sectra Fine';
