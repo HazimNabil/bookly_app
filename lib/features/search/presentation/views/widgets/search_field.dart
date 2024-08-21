781b39974545903df73792f2d89515a37040611d
HazimNabil <HazemNabil95@outlook.com>
2024-08-21 03:15:20 +0200

    Trigger search books cubit

--- lib/features/search/presentation/views/widgets/search_field.dart
+++ lib/features/search/presentation/views/widgets/search_field.dart
@@ -1,16 +1,27 @@
+import 'package:bookly_app/features/search/presentation/manager/search_books_cubit/search_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchField extends StatelessWidget {
-  const SearchField({super.key});
+  final controller = TextEditingController();
+
+  SearchField({super.key});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
       decoration: InputDecoration(
         hintText: 'Search book',
         suffixIcon: IconButton(
-          onPressed: () {},
+          onPressed: () {
+            BlocProvider.of<SearchBooksCubit>(context).searchBooks(
+              controller.text,
+            );
+            FocusScope.of(context).unfocus();
+            controller.clear();
+          },
           icon: const Opacity(
             opacity: 0.8,
             child: Icon(
