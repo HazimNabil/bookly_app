90d52fd0fdd36964a80ea0b96eae81bf888b7e52
HazimNabil <HazemNabil95@outlook.com>
2024-08-21 03:44:27 +0200

    Validate book title

--- lib/features/search/presentation/views/widgets/search_field.dart
+++ lib/features/search/presentation/views/widgets/search_field.dart
@@ -3,37 +3,66 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SearchField extends StatelessWidget {
+class SearchField extends StatefulWidget {
+  const SearchField({super.key});
+
+  @override
+  State<SearchField> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<SearchField> {
   final controller = TextEditingController();
 
-  SearchField({super.key});
+  final formKey = GlobalKey<FormState>();
+
+  var validateMode = AutovalidateMode.disabled;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        hintText: 'Search book',
-        suffixIcon: IconButton(
-          onPressed: () {
-            BlocProvider.of<SearchBooksCubit>(context).searchBooks(
-              controller.text,
-            );
-            FocusScope.of(context).unfocus();
-            controller.clear();
-          },
-          icon: const Opacity(
-            opacity: 0.8,
-            child: Icon(
-              FontAwesomeIcons.magnifyingGlass,
-              size: 20,
+    return Form(
+      key: formKey,
+      autovalidateMode: validateMode,
+      child: TextFormField(
+        controller: controller,
+        validator: validateBookTitle,
+        decoration: InputDecoration(
+          hintText: 'Search book',
+          suffixIcon: IconButton(
+            onPressed: () => onSearchButtonPressed(context),
+            icon: const Opacity(
+              opacity: 0.8,
+              child: Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 20,
+              ),
             ),
           ),
+          enabledBorder: buildOutlineBorder(),
+          focusedBorder: buildOutlineBorder(),
+          border: buildOutlineBorder(),
         ),
-        enabledBorder: buildOutlineBorder(),
-        focusedBorder: buildOutlineBorder(),
       ),
     );
+  }
+
+  void onSearchButtonPressed(BuildContext context) {
+    if (formKey.currentState!.validate()) {
+      BlocProvider.of<SearchBooksCubit>(context).searchBooks(
+        controller.text,
+      );
+      FocusScope.of(context).unfocus();
+      controller.clear();
+      setState(() => validateMode = AutovalidateMode.disabled);
+    } else {
+      setState(() => validateMode = AutovalidateMode.always);
+    }
+  }
+
+  String? validateBookTitle(value) {
+    if (value?.isEmpty ?? true) {
+      return 'Please enter a book title before searching';
+    }
+    return null;
   }
 
   OutlineInputBorder buildOutlineBorder() {
