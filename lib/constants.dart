602a8425e2f507d58e0bd3ad220599543c4d5dce
HazimNabil <HazemNabil95@outlook.com>
2024-09-01 19:24:24 +0200

    Change the app theme color

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFF100B20);
-const kShimmerBaseColor = Color(0xFF171525);
-const kShimmerHighlightColor = Color(0xFF1E1C33);
-const kTransitionDuration = Duration(milliseconds: 250);
+const kPrimaryColor = Color(0xFF102045);
+const kShimmerBaseColor = Color(0xFF172D4A);
+const kShimmerHighlightColor = Color(0xFF1E3C5F);
+
 const kGtSectraFine = 'GT Sectra Fine';
